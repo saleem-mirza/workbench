35062cae117437158c1b4c6c6ac718129145a872
Saleem Mirza <saleem-mirza@users.noreply.github.com>
2021-09-23 21:56:02 -0400

    add alternative recipe source

--- yelb-appserver/src/utils.rs
+++ yelb-appserver/src/utils.rs
@@ -6,12 +6,35 @@ static mut HOST: String = String::new();
 
 pub async fn get_recipe_url(item: &str) -> String {
     let client = Client::default();
-    let url = format!("http://www.recipepuppy.com/api/?q={}", item);
+    let url = format!(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s={}",
+        item
+    );
+
     match client.get(url).send().await {
         Ok(mut result) => match result.body().await {
             Ok(data) => {
                 let content: serde_json::Value = serde_json::from_slice(data.as_ref()).unwrap();
-                content["results"][0]["href"].to_string().replace("\"", "")
+                if !&content.is_object() {
+                    return "".to_string();
+                }
+
+                let recipes = &content["meals"][0];
+                if recipes.is_object() {
+                    let url = match recipes.as_object() {
+                        Some(recipe) => {
+                            if recipe.contains_key("strYoutube") {
+                                recipe["strYoutube"].to_string()
+                            } else {
+                                recipe["strSource"].to_string()
+                            }
+                        }
+                        None => String::from(""),
+                    };
+                    return url;
+                } else {
+                    String::from("")
+                }
             }
             Err(e) => {
                 eprintln!("{}", e.to_string());
